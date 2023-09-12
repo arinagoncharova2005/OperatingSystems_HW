9ad174becffd90ee3e09a75d9e799e8856443cd4
Arina Goncharova <arinagoncharova2005@yandex.ru>
2023-09-12 16:50:17 +0000

    first homework done

--- week01/ex2.sh
+++ week01/ex2.sh
@@ -1,1 +1,7 @@
+man cat
+grep "a" ~/Desktop/tt1.txt
+ls > ~/Desktop/filenames.txt
+echo "Today is sunny"
+less ~/week01/ex1.txt
+cat ~/Desktop/filenames.txt ~/Desktop/notes.txt > merged.txt
 history > ~/week01/ex2.txt
--- week01/ex3.sh
+++ week01/ex3.sh
@@ -1,10 +1,15 @@
 mkdir ~/week01/first_folder
+date
+sleep 3
 touch ~/week01/first_folder/root.txt
 date
 sleep 3
 mkdir ~/week01/second_folder
+date
+sleep 3
 touch ~/week01/second_folder/home.txt
 ls / -t -r > ~/week01/first_folder/root.txt
 ls ~ -t -r > ~/week01/second_folder/home.txt
 cat ~/week01/first_folder/root.txt ~/week01/second_folder/home.txt
-
+ls ~/week01/first_folder
+ls ~/week01/second_folder
